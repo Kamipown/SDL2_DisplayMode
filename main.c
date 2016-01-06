e1768f820e373593b5039bcb66f3c3a26b9360a1
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-06 12:50:13 +0100

    Change main.c

--- main.c
+++ main.c
@@ -2,26 +2,68 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
-// You have to init SDL_VIDEO to use these functions.
-
-void	print_displays_number()
+void	get_display_info(int displayNumber)
 {
-	printf("Number of Display: %d\n", SDL_GetNumVideoDisplays());
+	int i;
+	int display_mode_count;
+	SDL_DisplayMode mode;
+	Uint32 f;
+
+	display_mode_count = SDL_GetNumDisplayModes(displayNumber);
+	if (display_mode_count < 1)
+	{
+		printf("SDL_GetNumDisplayModes failed: %s\n", SDL_GetError());
+		return ;
+	}
+
+	printf("-----------------------------------------------------------------------\n");
+	printf("Informations for the display number %i :\n", displayNumber);
+	printf("%i Display Modes\n", display_mode_count);
+
+	i = 0;
+	while (i < display_mode_count)
+	{
+		if (SDL_GetDisplayMode(displayNumber, i, &mode) != 0)
+		{
+			printf("SDL_GetDisplayMode failed: %s\n", SDL_GetError());
+			return ;
+		}
+
+		f = mode.format;
+
+		printf("Mode %i\t", i);
+		printf("Bpp %i\t", SDL_BITSPERPIXEL(f));
+		printf("%s\t", SDL_GetPixelFormatName(f));
+		printf("%i x %i\t", mode.w, mode.h);
+		printf("Refresh Rate %i\n", mode.refresh_rate);
+		++i;
+	}
 }
 
 int		main(int argc, char *argv[])
 {
-	SDL_DisplayMode	*mode;
+	int numVideoDisplays;
+	int i;
 
 	SDL_SetMainReady();
-	SDL_Init(SDL_INIT_VIDEO);
 
-	print_displays_number();
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		printf("SDL_Init failed: %s\n", SDL_GetError());
+		return (1);
+	}
 
-	SDL_GetDisplayMode(0, 0, mode);
-	printf("Screen width : %d\n", mode->w);
-	printf("Screen height : %d\n", mode->h);
- 	printf("Hertz : %d\n", mode->refresh_rate);
+	if ((numVideoDisplays = SDL_GetNumVideoDisplays()) < 1)
+	{
+		printf("SDL_GetNumVideoDisplays failed: %s\n", SDL_GetError());
+		return (1);
+	}
+
+	printf("%i displays.\n\n", numVideoDisplays);
+
+	i = 0;
+	while (i < numVideoDisplays)
+		get_display_info(i++);
 
 	SDL_Quit();
 	return (0);
